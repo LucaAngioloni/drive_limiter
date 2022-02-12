b39a80ddd1c425ab21b67f49910d1019a65d23ab
Luca Angioloni <lucaangioloni@gmail.com>
2022-02-12 12:13:56 +0100

    Updated for the new Google Drive

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # drive_limiter
-drive_limiter limits the amount of CPU used by Backup and Sync by Google Drive.
+drive_limiter limits the amount of CPU used by  Google Drive.
--- drive_limiter.sh
+++ drive_limiter.sh
@@ -4,7 +4,10 @@
 sleep 30
 
 # get pid
-DRIVE_PID=$(ps -A | grep "Backup and Sync.app/Contents/MacOS/Backup" | awk '{print $1}' | head -1)
+DRIVE_PID=$(ps -A | grep "Google Drive.app/Contents/MacOS/Google Drive" | awk '{print $1}') # | head -1
 
 # limit cpu usage
-cpulimit --pid $DRIVE_PID --limit 55
+# cpulimit --pid $DRIVE_PID --limit 55
+echo $DRIVE_PID | while read pid ; do
+    cpulimit --pid $pid --limit 55
+done

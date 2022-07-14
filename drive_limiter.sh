58dcc37c39dc21a0f4756158edf6276d54c1c26e
Luca Angioloni <lucaangioloni@gmail.com>
2022-07-14 15:48:20 +0200

    Added while loop to wait 500 secs for the startup.

--- drive_limiter.sh
+++ drive_limiter.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 #
 # wait for the process to start
-sleep 40
+sleep 30
 
-# get pid
-DRIVE_PID=$(ps -A | grep "Google Drive.app/Contents/MacOS/Google Drive" | awk '{print $1}') # | head -1
+i=0
+
+while [[ $i -lt 5 ]]
+do
+  # get pid
+  DRIVE_PID=$(ps -A | grep "Google Drive.app/Contents/MacOS/Google Drive" | awk '{print $1}') # | head -1
+  LEN=$(echo $DRIVE_PID | wc -l | awk '{print $1}')
+  ((i++))
+  if [[ $i -gt 100 || $LEN -gt 1 ]]; then
+    break
+  fi
+  sleep 5
+done
 
 # limit cpu usage
 # cpulimit --pid $DRIVE_PID --limit 55
